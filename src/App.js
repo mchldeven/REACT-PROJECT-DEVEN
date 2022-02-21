3077edc81e31cca9e3f36c5e59b479a02f755876
michaeldeven <michael.deven1999@gmail.com>
2022-02-21 16:28:51 +0700

    GAS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './Commponents/Header';
 import Feature from './Commponents/Feature';
+import About from './Commponents/About';
+import aboutimage from './images/mantap2.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Feature />
+      <About image={aboutimage} title="Mudah Digunakan" button="Selengkapnya" />
+      <About image={aboutimage} title="Efesien!" button="get the app" />
     </div>
   );
 }
