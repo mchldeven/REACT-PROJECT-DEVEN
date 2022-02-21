bf637ff4351168263a2d5d4990c08fe6b8ddbd94
michaeldeven <michael.deven1999@gmail.com>
2022-02-21 13:05:32 +0700

    first commit

---
+++ src/Commponents/Navabar.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import logo from '../images/logo.png';
+
+function Navbar() {
+  const [nav, setnav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setnav(true);
+    } else {
+      setnav(fasle);
+    }
+  };
+  window.addEventListener('scroll', changeBackground);
+
+  return (
+    <nav className={nav ? 'nav active' : 'nav'}>
+      <a href="#" className="logo">
+        <img src={logo} alt="" />
+      </a>
+    </nav>
+  );
+}
+export default Navbar;
